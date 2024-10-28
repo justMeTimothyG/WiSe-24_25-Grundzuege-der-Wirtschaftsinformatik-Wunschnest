a533745a2d03aff193d749ab855c0ae252f7cc7f
Timothy Lüdtke <timothyludtke@gmail.com>
2024-10-28 14:27:40 +0100

    Projektarbeit beginnen, Tailwindcss einrichten

---
+++ src/Wunschnest/src/index.php
@@ -0,0 +1,19 @@
+<?php
+# Variablen definieren für die Homepage
+$title = "WunschNest"
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title><?php echo $title ?></title>
+</head>
+<body>
+    <h1 class="text-sky-600 text-3xl"><?php echo $title ?></h1>
+</body>
+</html>
