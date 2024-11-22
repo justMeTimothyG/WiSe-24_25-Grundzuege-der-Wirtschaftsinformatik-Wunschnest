9935bb31c6690a3ad7ae342d012d03c0856b797b
Timothy Lüdtke <timothyludtke@gmail.com>
2024-11-22 12:52:40 +0100

    fix link wunsch hinzufügen

--- Wunschnest/src/app/views/wishlist.php
+++ Wunschnest/src/app/views/wishlist.php
@@ -209,7 +209,9 @@ include BASE_PATH . '/components/includes/basic-head.php';
 
 
                     <div class="inline-flex rounded-md items-center shadow-sm" role="group">
-                        <button type="button" class="py-2 px-4 text-sm font-small text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 dark:hover:text-white dark:hover:bg-green-700">Wunsch hinzufügen</button>
+                        <a href="index.php?page=create&type=wish&wishlist_id=<?php echo $wishlist['wishlist_id'] ?>">
+                            <button type="button" class="py-2 px-4 text-sm font-small text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 dark:hover:text-white dark:hover:bg-green-700">Wunsch hinzufügen</button>
+                        </a>
                     </div>
                 </div>
                 <!-- Tabelle der Wünsche -->
